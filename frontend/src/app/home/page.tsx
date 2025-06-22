bfe916fc4e51aa82fbc0b76d3f93510ad351089d
Abhee <abhaynimbalkar03@gmail.com>
2025-06-23 01:37:11 +0530

    updated a minor change

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 
 interface Scan {
   id: number;
-  image_url: string;
+  public_url: string;
   created_at: string;
   detection_count: number;
   detections: Array<{
